ada1dc160b3d652c78145adcd36406147893ab2f
William Hunt <bill.hunt@clearavenue.com>
2020-02-10 14:06:48 -0500

    testing more

--- vars/devPipeline.groovy
+++ vars/devPipeline.groovy
@@ -47,13 +47,10 @@ spec:
 			stage('Build') {
 				steps {
 					container('maven') {
-						sh "echo GIT_COMMIT : ${env.GIT_COMMIT}"
-						sh "echo GIT_COMMITTER_NAME : ${env.GIT_COMMITTER_NAME}"
-						sh "echo GIT_COMMITTER_EMAIL : ${env.GIT_COMMITTER_EMAIL}"
-						sh "echo GIT_COMMITTER_DATE : ${env.GIT_COMMITTER_DATE}"
-						sh "echo GIT_URL : ${env.GIT_URL}"
-						sh "echo GIT_BRANCH : ${env.GIT_BRANCH}"
-						sh "echo GIT_LOCAL_BRANCH : ${env.GIT_LOCAL_BRANCH}"
+						script {
+							AUTHOR_NAME = sh (script: "git show -s --format='%%an' HEAD", returnStdout: true).split('\r\n')[2].trim()
+						}
+						sh "echo AUTHOR_NAME : ${AUTHOR_NAME}"
 					}
 				}
 			}
