0f0227407252b0a7c9837e95fcdc0fc96b783c93
William Hunt <bill.hunt@clearavenue.com>
2020-02-10 15:42:02 -0500

    testing more

--- vars/devPipeline.groovy
+++ vars/devPipeline.groovy
@@ -50,9 +50,13 @@ spec:
 					container('kubectl') {
 						script {
 							withKubeConfig([credentialsId: 'kube-admin', serverUrl: 'http://aa2e7b27c1cd44b91be7df2d25925337-1841660522.us-east-1.elb.amazonaws.com']) {
-								def NS = sh(returnStdout: true, script: "echo $COMMITTER_EMAIL | sed 's/@.*//' | sed 's/\\./-/g'")
-								NS=NS.trim()
+								def NS = sh(returnStdout: true, script: "echo $COMMITTER_EMAIL | sed 's/@.*//' | sed 's/\\./-/g'").trim()
 
+								sh "sed -i 's/APP_NAME/${pipelineParams.app_name}/g' deployment.yaml"
+								sh "sed -i 's/IMAGE_NAME/${pipelineParams.docker_user}\/${pipelineParams.service_name}/' deployment.yaml"
+								sh "sed -i 's/SERVICE_PORT/${pipelineParams.service_port}/g' deployment.yaml"
+								sh "sed -i 's/LIVENESS_URL/${pipelineParams.liveness_url}/g' deployment.yaml"
+								sh "sed -i 's/READINESS_URL/${pipelineParams.readiness_url}/g' deployment.yaml"
 								sh "sed -i 's/:latest/:${VERSION}/' deployment.yaml"
 								sh "sed -i 's/NAMESPACE/$NS/g' deployment.yaml"
 								sh "cat deployment.yaml"
