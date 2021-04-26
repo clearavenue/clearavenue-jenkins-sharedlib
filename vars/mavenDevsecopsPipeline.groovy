99c77e8a295ff002b366ebc458bb4b1c8df922ff
Travis Rieger <travis.rieger@clearavenue.com>
2021-04-26 20:46:35 +0000

    Update mavenDevsecopsPipeline.groovy

--- vars/mavenDevsecopsPipeline.groovy
+++ vars/mavenDevsecopsPipeline.groovy
@@ -140,7 +140,7 @@ spec:
 				steps {
 					container('kubectl') {
 						script {
-							withKubeConfig([credentialsId: 'kube-admin', serverUrl: 'https://ad148a8e1ef8041028aebf9835d5f358-192780462.us-east-1.elb.amazonaws.com']) {
+							withKubeConfig([credentialsId: 'kube-admin', serverUrl: 'https://10.0.0.113:9345']) {
 								
 								VERSION = ((env.GIT_BRANCH != 'master') ? "$POM_VERSION.$BUILD_NUMBER-$BRANCH" : "$POM_VERSION.$BUILD_NUMBER")
 
