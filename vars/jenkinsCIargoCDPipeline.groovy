f9911456fcb2aae01a2f31c338af6c26e63133e5
William Hunt <bill.hunt@clearavenue.com>
2022-01-25 14:00:35 -0500

    removed wrong tabs

--- vars/jenkinsCIargoCDPipeline.groovy
+++ vars/jenkinsCIargoCDPipeline.groovy
@@ -18,9 +18,9 @@ spec:
   - name: git
     image: bitnami/git:latest
 	command:
-	- cat
-	tty: true
-	resources:
+    - cat
+    tty: true
+    resources:
       requests:
         ephemeral-storage: 1Gi
       limits:
