654ef619d73c99659179bf899997a9c922b1453e
William Hunt <bill.hunt@clearavenue.com>
2022-02-10 16:57:54 -0500

    trying docker rootless

--- vars/jenkinsCIargoCDNoDepCheckPipeline.groovy
+++ vars/jenkinsCIargoCDNoDepCheckPipeline.groovy
@@ -15,13 +15,6 @@ metadata:
     pipeline: mavenDevsecopsPipeline
 spec:
   containers:
-  - name: docker
-    image: docker:dind
-    securityContext:
-      privileged: true
-    command:
-    - cat
-    tty: true
   - name: maven
     image: maven:3.6.1-jdk-11-slim
     command:
@@ -153,11 +146,9 @@ spec:
                                 APP_BRANCH = APP_NAME+BRANCH_NAME
                             }
 
-                            sh "apt-get install curl -y"
-                            sh "curl -sSL https://get.docker.com/ | sh"
-                            sh "sudo groupadd docker"
-                            sh "sudo usermod -aG docker \$(whoami)"
-                            sh "sudo service docker start"
+                            sh "apt-get install curl apt-utils newuidmap newgidmap docker-ce-rootless-extras -y"
+                            //sh "curl -sSL https://get.docker.com/ | sh"
+                            sh "dockerd-rootless-setuptool.sh install"
                             sh "docker version"
                             
 //                            sh "mvn -B -e -T 1C package com.google.cloud.tools:jib-maven-plugin:3.2.0:build -Dimage=${DOCKER_CREDS_USR}/${APP_BRANCH}:${POM_VERSION}-${BUILD_NUM} -DskipTests -Djib.to.auth.username=${DOCKER_CREDS_USR} -Djib.to.auth.password=${DOCKER_CREDS_PSW} -Djib.allowInsecureRegistries=true"
