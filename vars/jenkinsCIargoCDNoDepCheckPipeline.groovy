faea405c708f3efff0fad6d47520ed33be6a1f4e
William Hunt <bill.hunt@clearavenue.com>
2022-02-10 17:01:39 -0500

    start dockerd

--- vars/jenkinsCIargoCDNoDepCheckPipeline.groovy
+++ vars/jenkinsCIargoCDNoDepCheckPipeline.groovy
@@ -146,9 +146,9 @@ spec:
                                 APP_BRANCH = APP_NAME+BRANCH_NAME
                             }
 
-                            sh "apt-get install curl apt-utils newuidmap newgidmap docker-ce-rootless-extras -y"
-                            //sh "curl -sSL https://get.docker.com/ | sh"
-                            sh "dockerd-rootless-setuptool.sh install"
+                            sh "apt-get install curl -y"
+                            sh "curl -sSL https://get.docker.com/ | sh"
+                            sh "dockerd &"
                             sh "docker version"
                             
 //                            sh "mvn -B -e -T 1C package com.google.cloud.tools:jib-maven-plugin:3.2.0:build -Dimage=${DOCKER_CREDS_USR}/${APP_BRANCH}:${POM_VERSION}-${BUILD_NUM} -DskipTests -Djib.to.auth.username=${DOCKER_CREDS_USR} -Djib.to.auth.password=${DOCKER_CREDS_PSW} -Djib.allowInsecureRegistries=true"
