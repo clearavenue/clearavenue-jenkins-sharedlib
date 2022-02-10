29042723c9b780c5aa889986af360e8ccf2e03bd
William Hunt <bill.hunt@clearavenue.com>
2022-02-10 08:58:19 -0500

    updated jib to 3.2.0

--- vars/jenkinsCIargoCDNoDepCheckPipeline.groovy
+++ vars/jenkinsCIargoCDNoDepCheckPipeline.groovy
@@ -143,7 +143,7 @@ spec:
                                 APP_BRANCH = APP_NAME+BRANCH_NAME
                             }
 
-                            sh "mvn -B -e -T 1C package com.google.cloud.tools:jib-maven-plugin:3.1.4:build -Dimage=${DOCKER_CREDS_USR}/${APP_BRANCH}:${POM_VERSION}-${BUILD_NUM} -DskipTests -Djib.to.auth.username=${DOCKER_CREDS_USR} -Djib.to.auth.password=${DOCKER_CREDS_PSW} -Djib.allowInsecureRegistries=true"
+                            sh "mvn -B -e -T 1C package com.google.cloud.tools:jib-maven-plugin:3.2.0:build -Dimage=${DOCKER_CREDS_USR}/${APP_BRANCH}:${POM_VERSION}-${BUILD_NUM} -DskipTests -Djib.to.auth.username=${DOCKER_CREDS_USR} -Djib.to.auth.password=${DOCKER_CREDS_PSW} -Djib.allowInsecureRegistries=true"
                         }
                     }
                 }
