eb4b1565e0067fe110729ecbf7e84b354ee529fb
William Hunt <bill.hunt@clearavenue.com>
2022-03-04 13:52:39 -0500

    dependency check failonerror false

--- vars/rrsPipeline.groovy
+++ vars/rrsPipeline.groovy
@@ -149,7 +149,7 @@ spec:
                     stage('Vulnerabilities') {
                         steps {
                             container('jhipster') {
-                                sh "./mvnw -B -e -T 1C org.owasp:dependency-check-maven:6.5.3:aggregate -Dformat=xml" // -DfailBuildOnCVSS=10"
+                                sh "./mvnw -B -e -T 1C org.owasp:dependency-check-maven:7.0.0:aggregate -Dformat=xml" -DfailOnError=false // -DfailBuildOnCVSS=10"
                             }
                         }
                         post {
