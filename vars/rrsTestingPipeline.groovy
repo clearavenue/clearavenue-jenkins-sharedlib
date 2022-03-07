ebd1ca67644e532520f5cf8865654c96c1a72653
William Hunt <bill.hunt@clearavenue.com>
2022-03-07 13:41:47 -0500

    added node container

--- vars/rrsTestingPipeline.groovy
+++ vars/rrsTestingPipeline.groovy
@@ -15,6 +15,18 @@ metadata:
     pipeline: rrsPipeline
 spec:
   containers:
+  - name: node
+    image 'node:lts-buster-slim'
+    securityContext:
+       privileged: true
+    command:
+    - cat
+    tty: true
+    resources:
+      requests:
+        ephemeral-storage: 1Gi
+      limits:
+        ephemeral-storage: 5Gi
   - name: jhipster
     image: jhipster/jhipster:v7.6.0
     securityContext:
@@ -74,9 +86,8 @@ spec:
 	
 			stage('508 Test') {
 				steps{
-					container('maven'){
+					container('node'){
 						sh '''
-                apt-get install npm -y
                 apt-get install gconf-service libasound2 libatk1.0-0 libatk-bridge2.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libglib2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 ca-certificates fonts-liberation libappindicator1 libnss3 lsb-release xdg-utils wget -y
                 npm install -g pa11y-ci pa11y-ci-reporter-html --unsafe-perm=true
                 pa11y-ci -c pa11y.json --json ${WEB_APP} | tee pa11y-ci-results.json
