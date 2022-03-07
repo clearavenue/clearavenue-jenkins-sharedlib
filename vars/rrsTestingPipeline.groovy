362af5dbc0b839b5979bc1d868ff898ce63761cf
William Hunt <bill.hunt@clearavenue.com>
2022-03-07 13:01:42 -0500

    added 508 to testing

--- vars/rrsTestingPipeline.groovy
+++ vars/rrsTestingPipeline.groovy
@@ -1,13 +1,13 @@
 def call(body) {
-    def pipelineParams = [:]
-    body.resolveStrategy = Closure.DELEGATE_FIRST
-    body.delegate = pipelineParams
-    body()
+	def pipelineParams = [:]
+	body.resolveStrategy = Closure.DELEGATE_FIRST
+	body.delegate = pipelineParams
+	body()
 
-    pipeline {
-        agent {
-            kubernetes {
-                yaml """
+	pipeline {
+		agent {
+			kubernetes {
+				yaml """
 apiVersion: v1
 kind: Pod
 metadata:
@@ -56,121 +56,51 @@ spec:
       limits:
         ephemeral-storage: 5Gi
 """
-            }
-        }
+			}
+		}
 
-        environment {
-            POM_VERSION=readMavenPom().getVersion()
-            BUILD_NUM=currentBuild.getNumber()
-            DOCKER_CREDS=credentials('docker')
-            GIT_CREDS=credentials('bill.hunt-github')
-            BRANCH = env.GIT_BRANCH.toLowerCase()
-            APP_BRANCH="dummy"
-        }
-
-        stages {
-            stage('Build') {
-                steps {
-                    container('jhipster') {
-                        script {
-                             BUILD_PROFILE=pipelineParams.buildProfile
-                             sh "chmod +x mvnw"
-                             sh "./mvnw -B -e -T 1C clean package ${BUILD_PROFILE} -DskipTests"
-                        }
-                    }
-                }
-            }  // end build
-
-            stage('JUnit') {
-                steps {
-                    container('jhipster') {
-                        sh "./mvnw -B -e -T 1C ${BUILD_PROFILE} test"
-                        //junit 'target/surefire-reports/**/*.xml'
-                    }
-                }
-            }  // end junit
-
-            stage('SecurityChecks') {
-                parallel {
-                    stage('Checkstyle code') {
-                        steps {
-                            container('jhipster') {
-                                sh "./mvnw -B -e -T 1C ${BUILD_PROFILE} org.apache.maven.plugins:maven-checkstyle-plugin:3.1.2:checkstyle -Dcheckstyle.config.location=google_checks.xml"
-                            }
-                        }
-                        post {
-                            always {
-                                recordIssues(enabledForFailure: false, tool: checkStyle(pattern: 'target/checkstyle-result.xml'))
-                            }
-                        }
-                    }
-
-                    //stage('CodeCoverage') {
-                    //    steps {
-                    //        container('jhipster') {
-                    //            sh "./mvnw -B -e -T 1C ${BUILD_PROFILE} org.jacoco:jacoco-maven-plugin:0.8.7:prepare-agent verify org.jacoco:jacoco-maven-plugin:0.8.7:report"
-                    //            jacoco(execPattern: 'target/jacoco.exec', classPattern: 'target/classes', sourcePattern: 'src/main/java', exclusionPattern: 'src/test*', changeBuildStatus: false,
-                    //                   minimumInstructionCoverage : '30', maximumInstructionCoverage : '31',
-                    //                   minimumBranchCoverage : '30', maximumBranchCoverage : '31',
-                    //                   minimumComplexityCoverage : '30', maximumComplexityCoverage : '31',
-                    //                   minimumLineCoverage : '30', maximumLineCoverage : '31',
-                    //                   minimumMethodCoverage : '30', maximumMethodCoverage : '31',
-                    //                   minimumClassCoverage : '30', maximumClassCoverage : '31')
-                    //        }
-                    //    }
-                    //}  // end codecoverage
-
-                    //stage('SpotBugs') {
-                    //    steps {
-                    //       container('jhipster') {
-                    //            sh "./mvnw -B -e -T 1C ${BUILD_PROFILE} com.github.spotbugs:spotbugs-maven-plugin:4.5.3.0:check -Dspotbugs.effort=Max -Dspotbugs.threshold=Low -Dspotbugs.failOnError=false"
-                    //        }
-                    //    }
-                    //    post {
-                    //        always {
-                    //            recordIssues(enabledForFailure: true, tool: spotBugs())
-                    //        }
-                    //    }
-                    //} // end spotbugs
-
-                    stage('PMD') {
-                        steps {
-                            container('jhipster') {
-                                sh "./mvnw -B -e ${BUILD_PROFILE} org.apache.maven.plugins:maven-jxr-plugin:3.1.1:jxr org.apache.maven.plugins:maven-pmd-plugin:3.14.0:pmd"
-                            }
-                        }
-                        post {
-                            always {
-                                recordIssues(enabledForFailure: true, tool: pmdParser(pattern: 'target/pmd.xml'))
-                            }
-                        }
-                    } // end pmd
-                  
-                    stage('Vulnerabilities') {
-                        steps {
-                            container('jhipster') {
-                                sh "./mvnw -B -e -T 1C ${BUILD_PROFILE} org.owasp:dependency-check-maven:7.0.0:aggregate -Dformat=xml -DfailOnError=false" // -DfailBuildOnCVSS=10"
-                            }
-                        }
-                        post {
-                            always {
-                                dependencyCheckPublisher(failedTotalCritical : 100, unstableTotalCritical : 100)
-                            }
-                        }
-                    } // end vulnerabilities
-                } // end parallel
-            } // end security checks
+		environment {
+			POM_VERSION=readMavenPom().getVersion()
+			BUILD_NUM=currentBuild.getNumber()
+			DOCKER_CREDS=credentials('docker')
+			GIT_CREDS=credentials('bill.hunt-github')
+			BRANCH = env.GIT_BRANCH.toLowerCase()
+			APP_BRANCH="dummy"
 			
-        } // end stages
+			WEB_APP="https://reservationapp.dev-devsecops.clearavenue.com"
+		}
 
-        post {
-            always {
-                emailext attachLog: true, subject: '$DEFAULT_SUBJECT', body: '$DEFAULT_CONTENT', recipientProviders: [
-                    [$class: 'CulpritsRecipientProvider'],
-                    [$class: 'DevelopersRecipientProvider'],
-                    [$class: 'RequesterRecipientProvider']
-                ]
-            }
-        }
-    }
+		stages {
+	
+			stage('508 Test') {
+				steps{
+					container('jhipster'){
+						sh '''
+                apt-get install npm -y
+                apt-get install gconf-service libasound2 libatk1.0-0 libatk-bridge2.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libglib2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 ca-certificates fonts-liberation libappindicator1 libnss3 lsb-release xdg-utils wget -y
+                npm install -g pa11y-ci pa11y-ci-reporter-html --unsafe-perm=true
+                pa11y-ci -c pa11y.json --json ${WEB_APP} | tee pa11y-ci-results.json
+                pa11y-ci-reporter-html -d pa11y-html
+              '''
+						publishHTML target: [allowMissing: false, alwaysLinkToLastBuild: true, keepAll: true,
+							reportDir: 'pa11y-html',
+							reportFiles: 'index.html',
+							reportName: "508 Test"
+						]
+					}
+				}
+			}   // end 508 tests
+			
+		} // end stages
+
+		post {
+			always {
+				emailext attachLog: true, subject: '$DEFAULT_SUBJECT', body: '$DEFAULT_CONTENT', recipientProviders: [
+					[$class: 'CulpritsRecipientProvider'],
+					[$class: 'DevelopersRecipientProvider'],
+					[$class: 'RequesterRecipientProvider']
+				]
+			}
+		}
+	}
 }
