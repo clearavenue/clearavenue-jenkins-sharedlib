c5ff5de484d4f1ca50ccb2e73d42302f987b06a8
William Hunt <bill.hunt@clearavenue.com>
2022-03-08 09:49:21 -0500

    added cypress container

--- vars/rrsTestingPipeline.groovy
+++ vars/rrsTestingPipeline.groovy
@@ -40,6 +40,19 @@ spec:
       limits:
         ephemeral-storage: 5Gi
 
+  - name: cypress
+    image: cypress/included:9.5.1
+    securityContext:
+      privileged: true
+    command:
+    - cat
+    tty: true
+    resources:
+      requests:
+        ephemeral-storage: 1Gi
+      limits:
+        ephemeral-storage: 5Gi
+
   - name: jnlp
     resources:
       requests:
@@ -81,7 +94,7 @@ spec:
 			
 			stage('Cypress Test') {
 				steps {
-					container('nodejs'){
+					container('cypress'){
 						sh '''
                                cd reservationapp
                                npx cypress run
