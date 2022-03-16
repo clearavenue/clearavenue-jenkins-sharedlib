7f4c1aac2151238f123cf6291fc8abde9bf49588
William Hunt <bill.hunt@clearavenue.com>
2022-03-16 11:39:02 -0400

    add 508 testing

--- vars/rrsDevPipeline.groovy
+++ vars/rrsDevPipeline.groovy
@@ -377,6 +377,24 @@ spec:
 				}
 			} // end jest tests
 
+                        stage('508 Test') {
+                           when {
+                                  expression {
+                                      return INTEGRATION_TESTING == 'true'
+                                  }
+                           }
+                           steps {
+                                container('nodejs') {
+                                    sh '''
+                                       APP_ELB=`cat app-elb.txt`
+                                       apt-get install gconf-service libasound2 libatk1.0-0 libatk-bridge2.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libglib2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 ca-certificates fonts-liberation libappindicator1 libnss3 lsb-release xdg-utils wget -y
+                                       npm install -g pa11y-ci pa11y-ci-reporter-html --unsafe-perm=true
+                                       pa11y-ci -c pa11y.json --json http://${APP_ELB}:80 | tee pa11y-ci-results.json
+                                       pa11y-ci-reporter-html -d pa11y-html
+                                   '''
+                                 }
+                           }
+                        }  // end 508
 
 			stage('Cypress Test') {
 				when {
